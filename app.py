8ce434de3246e97078912c7eae743d0337f07e1c
Gurupra5ad <guruprasad2511@gmail.com>
2021-02-23 21:19:25 +0530

    Finished list based book management

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ USER_CHOICE = """
 Enter :
  - 'a' to add a new book
  - 'l' to list all the books
- - 'r' to remove a book
+ - 'r' to mark a book as read
  - 'd' to delete a book
  - 'q' to quit 
 Your choice ? : """
@@ -12,9 +12,40 @@ Your choice ? : """
 def menu():
     user_input = input(USER_CHOICE)
     while user_input != 'q':
-        pass
+        if user_input == 'a':
+            prompt_add_book()
+        elif user_input == 'l':
+            list_books()
+        elif user_input == 'r':
+            prompt_read_book()
+        elif user_input == 'd':
+            prompt_delete_book()
+        else:
+            print("Unknown input given, please try again. ")
+        
+        user_input = input(USER_CHOICE)
 
-#def prompt_add_book() ask for book name and add a book
-#def list_books() show all the books in our list
-#def promp_read_book() ask for book name and modify it to read in our list 
-#def remove_book() ask for book name and delete it from our list
+
+def prompt_add_book():
+    name = input("Enter the book name : ")
+    author = input("Enter the author name: ")
+
+    database.add_book(name,author)
+
+
+def list_books():
+    books = database.get_all_books()
+    for book in books:
+        read = 'YES' if book['read'] else 'NO'
+        print(f"{book['name']} by {book['author']}, read : {read}")
+
+def prompt_read_book():
+    name = input("Enter the name of the book that u want to change to read : ")
+    database.mark_book_read(name)
+
+
+def prompt_delete_book():
+    name = input("Enter the name of the book that u want to delete : ")
+    database.delete_book(name)
+
+menu()
--- utils/database.py
+++ utils/database.py
@@ -6,3 +6,17 @@ books = []
 
 def add_book(name, author):
     books.append({'name':name, 'author':author, 'read': False})
+
+
+def get_all_books():
+    return books
+
+
+def mark_book_read(name):
+    for book in books:
+        if book['name'] == name:
+            book['read'] = True
+
+def delete_book(name):
+    global books 
+    books = [book for book in books if book['name'] != name] #add each book to new books list if name is not equal to the name that needs to be deleted
