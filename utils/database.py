755a60e3b3a1f4a2ff86d84646d6e2b2bed433f5
Gurupra5ad <guruprasad2511@gmail.com>
2021-02-24 23:10:32 +0530

    Added database and created v3 of the project

--- utils/database.py
+++ utils/database.py
@@ -1,41 +1,45 @@
-import json
-
+from .database_connection import DatabaseConnection
 """ 
         Concerned with storing and retreiving books from the list
 """
 
-books_file = 'books.json'
-
 
 def create_book_table():
-    with open(books_file, 'w') as file:
-        json.dump([], file)
+    with DatabaseConnection('data.db') as connection:
+        cursor = connection.cursor()
+
+        cursor.execute('CREATE TABLE IF NOT EXISTS books(name text primary key, author text, read integer)')
 
 def add_book(name, author):
-    books = get_all_books()
-    books.append({'name':name, 'author':author, 'read':False})
-    _save_all_books_(books)
+    with DatabaseConnection('data.db') as connection:
+
+        cursor = connection.cursor()
+
+        cursor.execute('INSERT INTO books VALUES(?,?,0)', (name,author))
+
 
 def get_all_books():
-    with open(books_file, 'r') as file:
-        return json.load(file)
+    with DatabaseConnection('data.db') as connection:
 
+        cursor = connection.cursor()
 
-def _save_all_books_(books):
-    with open (books_file, 'w') as file:
-        json.dump(books, file)
+        cursor.execute('SELECT * FROM books')
+        books = [{'name':row[0], 'author':row[1], 'read':row[2]} for row in cursor.fetchall()]
 
+        return books
 
 
 def mark_book_read(name):
-    books = get_all_books()
-    for book in books:
-        if book['name'] == name:
-            book['read'] = True
-        _save_all_books_(books)
+    with DatabaseConnection('data.db') as connection:
+
+        cursor = connection.cursor()
+
+        cursor.execute('UPDATE books SET read=1 WHERE name = ?', (name,))
 
 
 def delete_book(name):
-    books = get_all_books() 
-    books = [book for book in books if book['name'] != name]#add each book to new books list if name is not equal to the name that needs to be deleted
-    _save_all_books_(books)
+    with DatabaseConnection('data.db') as connection:
+
+        cursor = connection.cursor()
+
+        cursor.execute('DELETE FROM books WHERE name = ?', (name,))
